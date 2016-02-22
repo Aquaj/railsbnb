c706e25b3948d73b8feae82cd0b8510bf6eeaee1
Jeremie <jeremie.bonal@gmail.com>
2016-02-22 19:21:52 +0100

    Controller for Flats created.

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,39 @@
+class FlatsController < ApplicationController
+    before_action :find_flat, only: [:edit, :update, :destroy, :show]
+
+    def index
+      @flats = Flat.all
+    end
+
+    def show
+    end
+
+    def new
+      @flat = Flat.new
+    end
+
+    def create
+      @flat = Flat.new(flat_params)
+    end
+
+    def update
+    end
+
+    def edit
+    end
+
+    def destroy
+      @flat.destroy
+    end
+
+private
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:user_id, :address, :description)
+  end
+
+end
