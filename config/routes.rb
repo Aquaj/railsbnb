7dad43214ea7d10130d7fec9dc9c747ba4be9c41
Jeremie <jeremie.bonal@gmail.com>
2016-02-22 18:25:43 +0100

    Added routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :flats, only: [:index, :show, :new, :create, :destroy]
+  resources :users, only: [:show, :edit, :new, :create] do
+    resources :bookings, only: [:index, :show, :new, :create, :destroy]
+    get 'flats' => "users#show_flats"
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
